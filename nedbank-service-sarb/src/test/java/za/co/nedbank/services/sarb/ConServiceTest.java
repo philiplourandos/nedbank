0d5081ba56c3aacb06b1530299f5b403e4012f94
Philip Lourandos <philip.lourandos@gmail.com>
2021-12-01 10:19:10 +0200

    #3
    * Added test for cron service

---
+++ nedbank-service-sarb/src/test/java/za/co/nedbank/services/sarb/ConServiceTest.java
@@ -0,0 +1,16 @@
+package za.co.nedbank.services.sarb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+@ActiveProfiles({"test"})
+public class ConServiceTest {
+    @Test
+    public void givenConfiguredCron_whenTrigger_thenRetrieveSarbValuesAndCache() throws Exception {
+        fail();
+    }
+}
