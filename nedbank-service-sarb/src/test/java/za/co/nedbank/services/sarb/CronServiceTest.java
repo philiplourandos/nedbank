385d6a4a3e784c6fbf20817f3c8a297ee1aabb72
Philip Lourandos <philip.lourandos@gmail.com>
2021-12-02 20:00:32 +0200

    #5
    * Implemented test case for cron scheduling and retrieval of data

--- nedbank-service-sarb/src/test/java/za/co/nedbank/services/sarb/CronServiceTest.java
+++ nedbank-service-sarb/src/test/java/za/co/nedbank/services/sarb/CronServiceTest.java
@@ -1,16 +1,45 @@
 package za.co.nedbank.services.sarb;
 
+import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
 @ActiveProfiles({"test"})
+@AutoConfigureWireMock(stubs = {"classpath:/stubs"})
 public class CronServiceTest {
+    
+    @Autowired
+    private CacheManager manager;
+    
+    @Value("${spring.cache.cache-names}")
+    private String cacheName;
+    
     @Test
     public void givenConfiguredCron_whenTrigger_thenRetrieveSarbValuesAndCache() throws Exception {
-        fail();
+        //given
+        final BigDecimal cpi = new BigDecimal("5.0000");
+        final BigDecimal ppi = new BigDecimal("8.1162");
+        final BigDecimal repo = new BigDecimal("3.7500");
+        final BigDecimal prime = new BigDecimal("7.2500");
+
+        final Cache sarbCache = manager.getCache(cacheName);
+
+        await().atMost(300, TimeUnit.SECONDS).untilAsserted(() -> {
+            assertEquals(cpi, sarbCache.get(RateEnum.CPI.getCacheKey(), BigDecimal.class));
+            assertEquals(ppi, sarbCache.get(RateEnum.PPI.getCacheKey(), BigDecimal.class));
+            assertEquals(repo, sarbCache.get(RateEnum.REPO.getCacheKey(), BigDecimal.class));
+            assertEquals(prime, sarbCache.get(RateEnum.PRIME.getCacheKey(), BigDecimal.class));
+        });
     }
 }
