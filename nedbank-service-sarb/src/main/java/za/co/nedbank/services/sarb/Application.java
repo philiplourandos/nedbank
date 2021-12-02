1998e6153c644c67617f4765646416527daf5313
Philip Lourandos <philip.lourandos@gmail.com>
2021-12-02 14:08:07 +0200

    #5
    * Added bootstrap class

---
+++ nedbank-service-sarb/src/main/java/za/co/nedbank/services/sarb/Application.java
@@ -0,0 +1,14 @@
+package za.co.nedbank.services.sarb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
