b5e0e23a266e147e607c998cfde83bd0562aef13
ThapeloC <chabangut@gmail.com>
2021-12-05 18:02:28 +0200

    #7 \n\n *Added method to retrieve rates on application startup

--- nedbank-service-sarb/src/main/java/za/co/nedbank/services/sarb/Application.java
+++ nedbank-service-sarb/src/main/java/za/co/nedbank/services/sarb/Application.java
@@ -2,9 +2,12 @@ package za.co.nedbank.services.sarb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import za.co.nedbank.services.sarb.service.SarbService;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -14,5 +17,12 @@ public class Application {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    //Tells Spring to do this once application is ready to start running
+    @EventListener(ApplicationReadyEvent.class)
+    public void onStart(final SarbService service){
+        service.retrieveRates();
+
     }
 }
