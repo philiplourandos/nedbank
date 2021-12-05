a09d5d05c1f1ade16ab0671336d0d3c4e3c5703a
ThapeloC <chabangut@gmail.com>
2021-12-05 19:48:05 +0200

    #6 \n\n* Added TestConst file

---
+++ nedbank-service-sarb/src/test/java/za/co/nedbank/services/sarb/TestConst.java
@@ -0,0 +1,10 @@
+ package za.co.nedbank.services.sarb;
+ 
+ public final class TestConst{
+ 
+	public static final BigDecimal CPI = new BigDecimal("5.0000");
+	public static final BigDecimal PPI = new BigDecimal("8.1162");
+	public static final BigDecimal REPO = new BigDecimal("3.7500");
+	public static final BigDecimal PRIME = new BigDecimal("7.2500");
+ 
+ }
