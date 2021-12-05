de54a37f39e5ceb70ffbf0b5d66e234a1fc1eb90
Philip Lourandos <philip.lourandos@gmail.com>
2021-12-05 21:22:36 +0200

    #6
    * Use static imports

--- nedbank-service-sarb/src/main/java/za/co/nedbank/services/sarb/config/WebFluxConfig.java
+++ nedbank-service-sarb/src/main/java/za/co/nedbank/services/sarb/config/WebFluxConfig.java
@@ -3,17 +3,21 @@ package za.co.nedbank.services.sarb.config;
 import java.math.BigDecimal;
 import java.util.Optional;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import za.co.nedbank.services.sarb.RateEnum;
 import za.co.nedbank.services.sarb.model.RateResponse;
 import za.co.nedbank.services.sarb.service.SarbService;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
 public class WebFluxConfig {
     @Bean
-    public RouterFunction ratesRoute(final SarbService service) {
-        return RouterFunctions.route().GET("/sarb/rate/{type}", request -> {
+    public RouterFunction<ServerResponse> ratesRoute(final SarbService service) {
+        return route(GET("/sarb/rate/{type}"), request -> {
             final String requiredRate = request.pathVariable("type");
 
             try {
@@ -28,6 +32,6 @@ public class WebFluxConfig {
             } catch (Exception e) {
                 return ServerResponse.notFound().build();
             }
-        }).build();
+        });
     }
 }
