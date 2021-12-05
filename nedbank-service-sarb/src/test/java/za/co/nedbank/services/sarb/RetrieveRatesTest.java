691ff49ed92215c796f73256d35401b37ce0b2e4
ThapeloC <chabangut@gmail.com>
2021-12-05 19:50:14 +0200

    #6\n\n* Implemented the test cases for getRate

--- nedbank-service-sarb/src/test/java/za/co/nedbank/services/sarb/RetrieveRatesTest.java
+++ nedbank-service-sarb/src/test/java/za/co/nedbank/services/sarb/RetrieveRatesTest.java
@@ -1,8 +1,10 @@
 package za.co.nedbank.services.sarb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -10,23 +12,38 @@ import static org.junit.jupiter.api.Assertions.fail;
 @ActiveProfiles({"test"})
 public class RetrieveRatesTest {
 
+    @Autowired
+    private WebTestClient client;
+
     @Test
     public void givenLoadedRepoRate_whenRequestMade_thenReturnRepoRateAnd200() throws Exception {
-        fail();
+        client.get().uri("/sarb/rate/repo")
+        .expectStatus().isOk()
+		.expectBody()
+		.jsonPath("$.rate").isEqualTo(TestConst.REPO);
     }
     
     @Test
     public void givenLoadedPPIRate_whenRequestMade_thenReturnPPIRateAnd200() throws Exception {
-        fail();
+        client.get().uri("/sarb/rate/repo")
+        .expectStatus().isOk()
+		.expectBody()
+		.jsonPath("$.rate").isEqualTo(TestConst.PPI);
     }
 
     @Test
     public void givenLoadedCPIRate_whenRequestMade_thenReturnCPIRateAnd200() throws Exception {
-        fail();
+        client.get().uri("/sarb/rate/repo")
+        .expectStatus().isOk()
+		.expectBody()
+		.jsonPath("$.rate").isEqualTo(TestConst.CPI);
     }
 
     @Test
     public void givenLoadedPrimeRate_whenRequestMade_thenReturnPrimeRateAnd200() throws Exception {
-        fail();
+        client.get().uri("/sarb/rate/repo")
+        .expectStatus().isOk()
+		.expectBody()
+		.jsonPath("$.rate").isEqualTo(TestConst.PRIME);
     }
 }
