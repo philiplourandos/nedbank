995a6b611e77b40bd739324c953c9895c5711297
Philip Lourandos <philip.lourandos@gmail.com>
2021-12-05 20:27:38 +0200

    #6
    * Added `BigDecimal` import

--- nedbank-service-sarb/src/test/java/za/co/nedbank/services/sarb/TestConst.java
+++ nedbank-service-sarb/src/test/java/za/co/nedbank/services/sarb/TestConst.java
@@ -1,10 +1,13 @@
- package za.co.nedbank.services.sarb;
- 
- public final class TestConst{
- 
-	public static final BigDecimal CPI = new BigDecimal("5.0000");
-	public static final BigDecimal PPI = new BigDecimal("8.1162");
-	public static final BigDecimal REPO = new BigDecimal("3.7500");
-	public static final BigDecimal PRIME = new BigDecimal("7.2500");
- 
- }
+package za.co.nedbank.services.sarb;
+
+import java.math.BigDecimal;
+
+public final class TestConst {
+    public static final BigDecimal CPI = new BigDecimal("5.0000");
+    public static final BigDecimal PPI = new BigDecimal("8.1162");
+    public static final BigDecimal REPO = new BigDecimal("3.7500");
+    public static final BigDecimal PRIME = new BigDecimal("7.2500");
+
+    private TestConst() {
+    }
+}
