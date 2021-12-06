66bd0b9e1267f51c3ffb5bfb229c40463493cc79
Philip Lourandos <philip.lourandos@gmail.com>
2021-12-06 08:02:17 +0200

    #6
    * Removed mockito

--- nedbank-service-sarb/src/main/java/za/co/nedbank/services/sarb/config/WebFluxConfig.java
+++ nedbank-service-sarb/src/main/java/za/co/nedbank/services/sarb/config/WebFluxConfig.java
@@ -1,14 +1,10 @@
 package za.co.nedbank.services.sarb.config;
 
-import java.math.BigDecimal;
-import java.util.Optional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import za.co.nedbank.services.sarb.RateEnum;
-import za.co.nedbank.services.sarb.model.RateResponse;
-import za.co.nedbank.services.sarb.service.SarbService;
+import za.co.nedbank.services.sarb.handler.GetRateHandler;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -16,22 +12,7 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration(proxyBeanMethods = false)
 public class WebFluxConfig {
     @Bean
-    public RouterFunction<ServerResponse> ratesRoute(final SarbService service) {
-        return route(GET("/sarb/rate/{type}"), request -> {
-            final String requiredRate = request.pathVariable("type");
-
-            try {
-                final RateEnum required = RateEnum.valueOf(requiredRate.toUpperCase());
-                final Optional<BigDecimal> foundRate = service.getRate(required);
-
-                if (foundRate.isPresent()) {
-                    return ServerResponse.ok().bodyValue(new RateResponse(foundRate.get()));
-                } else {
-                    return ServerResponse.noContent().build();
-                }
-            } catch (Exception e) {
-                return ServerResponse.notFound().build();
-            }
-        });
+    public RouterFunction<ServerResponse> ratesRoute(final GetRateHandler requestHandler) {
+        return route(GET("/sarb/rate/{type}"), requestHandler::handle);
     }
 }
