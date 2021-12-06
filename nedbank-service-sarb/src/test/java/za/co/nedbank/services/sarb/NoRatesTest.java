0213cd9a27e6d95e3d1ec7c10f63afcabb501736
Philip Lourandos <philip.lourandos@gmail.com>
2021-12-06 08:28:49 +0200

    #6
    * Added test case if no rates are loaded return 204

--- nedbank-service-sarb/src/test/java/za/co/nedbank/services/sarb/NoRatesTest.java
+++ nedbank-service-sarb/src/test/java/za/co/nedbank/services/sarb/NoRatesTest.java
@@ -2,29 +2,49 @@ package za.co.nedbank.services.sarb;
 
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(properties = {"za.co.nedbank.service.sarb.url=8888"},
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles({"test"})
 public class NoRatesTest {
+
+    @Autowired
+    private WebTestClient client;
+
     @Test
     public void givenNoLoadedRepoRate_whenRequestMade_thenReturn204WithNoBody() throws Exception {
-        fail();
+        client.get().uri("/sarb/rate/repo")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNoContent();
     }
     
     @Test
     public void givenNoLoadedPPIRate_whenRequestMade_thenReturn204WithNoBody() throws Exception {
-        fail();
+        client.get().uri("/sarb/rate/ppi")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNoContent();
     }
 
     @Test
     public void givenNoLoadedCPIRate_whenRequestMade_thenReturn204WithNoBody() throws Exception {
-        fail();
+        client.get().uri("/sarb/rate/cpi")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNoContent();
     }
 
     @Test
     public void givenNoLoadedPrimeRate_whenRequestMade_thenReturn204WithNoBody() throws Exception {
-        fail();
+        client.get().uri("/sarb/rate/prime")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNoContent();
     }
 }
