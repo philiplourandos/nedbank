63a3fed742651539dc5699264f5e6343dded0c40
Philip Lourandos <philip.lourandos@gmail.com>
2021-12-06 08:29:38 +0200

    #6
    * Removed trailing zeros as they are removed on occassion

--- nedbank-service-sarb/src/test/java/za/co/nedbank/services/sarb/TestConst.java
+++ nedbank-service-sarb/src/test/java/za/co/nedbank/services/sarb/TestConst.java
@@ -3,10 +3,10 @@ package za.co.nedbank.services.sarb;
 import java.math.BigDecimal;
 
 public final class TestConst {
-    public static final BigDecimal CPI = new BigDecimal("5.0000");
+    public static final BigDecimal CPI = new BigDecimal("5.0");
     public static final BigDecimal PPI = new BigDecimal("8.1162");
-    public static final BigDecimal REPO = new BigDecimal("3.7500");
-    public static final BigDecimal PRIME = new BigDecimal("7.2500");
+    public static final BigDecimal REPO = new BigDecimal("3.75");
+    public static final BigDecimal PRIME = new BigDecimal("7.25");
 
     private TestConst() {
     }
