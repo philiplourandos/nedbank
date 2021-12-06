e2f1fb6916f55a6295403946d90967ca45f34976
Philip Lourandos <philip.lourandos@gmail.com>
2021-12-06 07:55:13 +0200

    #6
    * Removed mockito

--- nedbank-service-sarb/src/test/java/za/co/nedbank/services/sarb/RetrieveRatesTest.java
+++ nedbank-service-sarb/src/test/java/za/co/nedbank/services/sarb/RetrieveRatesTest.java
@@ -1,45 +1,46 @@
 package za.co.nedbank.services.sarb;
 
-import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import za.co.nedbank.services.sarb.service.SarbService;
-
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @WebFluxTest(properties = {"za.co.nedbank.service.sarb.url=http://localhost:${wiremock.server.port}"})
 @ActiveProfiles({"test"})
 public class RetrieveRatesTest {
 
-    @MockBean
-    private SarbService service;
-    
     @Autowired
     private WebTestClient client;
 
+    @Autowired
+    private CacheManager manager;
+    
+    @Value("${spring.cache.cache-names}")
+    private String cacheName;
+    
+    @BeforeEach
+    public void setup() {
+        final Cache sarbCache = manager.getCache(cacheName);
+        sarbCache.put(RateEnum.CPI.getCacheKey(), TestConst.CPI);
+        sarbCache.put(RateEnum.PPI.getCacheKey(), TestConst.PPI);
+        sarbCache.put(RateEnum.PRIME.getCacheKey(), TestConst.PRIME);
+        sarbCache.put(RateEnum.REPO.getCacheKey(), TestConst.REPO);
+    }
+
     @Test
     public void givenLoadedRepoRate_whenRequestMade_thenReturnRepoRateAnd200() throws Exception {
-        //given
-        when(service.getRate(eq(RateEnum.REPO))).thenReturn(Optional.of(TestConst.REPO));
-
-        //when
         client.get().uri("/sarb/rate/repo")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody()
                 .jsonPath("$.rate").isEqualTo(TestConst.REPO);
-
-        //then
-        verify(service, times(1)).getRate(eq(RateEnum.REPO));
     }
 
     @Test
