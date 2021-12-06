53383b75fa431b6ef43aa710b13b5713c8538049
Philip Lourandos <philip.lourandos@gmail.com>
2021-12-06 09:18:45 +0200

    #6
    * Added bean for openfeign http converter

--- nedbank-service-sarb/src/main/java/za/co/nedbank/services/sarb/config/WebFluxConfig.java
+++ nedbank-service-sarb/src/main/java/za/co/nedbank/services/sarb/config/WebFluxConfig.java
@@ -1,7 +1,15 @@
 package za.co.nedbank.services.sarb.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.codec.Decoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import za.co.nedbank.services.sarb.handler.GetRateHandler;
@@ -14,5 +22,13 @@ public class WebFluxConfig {
     @Bean
     public RouterFunction<ServerResponse> ratesRoute(final GetRateHandler requestHandler) {
         return route(GET("/sarb/rate/{type}"), requestHandler::handle);
+    }
+
+    @Bean
+    public Decoder feignDecoder(final ObjectMapper mapper) {
+        final HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter(mapper);
+        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
+
+        return new ResponseEntityDecoder(new SpringDecoder(objectFactory));
     }
 }
