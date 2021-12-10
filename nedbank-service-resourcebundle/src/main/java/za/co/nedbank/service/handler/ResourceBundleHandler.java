c2082b31d4b1bbfa449760f3228f8c05f3529f7a
Philip Lourandos <philip.lourandos@gmail.com>
2021-12-10 16:33:35 +0200

    * Handler for reactive endpoint

--- nedbank-service-resourcebundle/src/main/java/za/co/nedbank/service/handler/ResourceBundleHandler.java
+++ nedbank-service-resourcebundle/src/main/java/za/co/nedbank/service/handler/ResourceBundleHandler.java
@@ -1,11 +1,37 @@
 package za.co.nedbank.service.handler;
 
+import java.util.Locale;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+@Component
 public class ResourceBundleHandler {
-    public Mono<ServerResponse> getResourceBundleEntry(final ServerRequest request) {
-        
+
+    private static final String[] EMPTY_ARGS = {};
+
+    private final ReloadableResourceBundleMessageSource resourceBundle;
+
+    public ResourceBundleHandler(final ReloadableResourceBundleMessageSource resourceBundle) {
+        this.resourceBundle = resourceBundle;
     }
+
+    public Mono<ServerResponse> getResourceBundleEntry(final ServerRequest request) {
+        final String resourcKey = request.pathVariable("key");
+
+        try {
+            final String textValue = resourceBundle.getMessage(resourcKey, EMPTY_ARGS, Locale.ENGLISH);
+
+            return ServerResponse.ok().bodyValue(new ResourceBundleResponse(textValue));
+        } catch(NoSuchMessageException noMessage) {
+            return ServerResponse.notFound().build();
+        }
+    }
+}
+
+record ResourceBundleResponse(String text) {
+    
 }
