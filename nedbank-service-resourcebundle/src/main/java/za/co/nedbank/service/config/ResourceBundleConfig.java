54b06133b0cc29856513574307a48b9d9e4d3a8c
Philip Lourandos <philip.lourandos@gmail.com>
2021-12-10 16:34:20 +0200

    * Added configuration for resource bundle

--- nedbank-service-resourcebundle/src/main/java/za/co/nedbank/service/config/ResourceBundleConfig.java
+++ nedbank-service-resourcebundle/src/main/java/za/co/nedbank/service/config/ResourceBundleConfig.java
@@ -1,19 +1,35 @@
 package za.co.nedbank.service.config;
 
+import java.nio.charset.StandardCharsets;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import za.co.nedbank.service.handler.ResourceBundleHandler;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import za.co.nedbank.service.handler.ResourceBundleHandler;
 
 @Configuration
 public class ResourceBundleConfig {
-    
+
     @Bean
     public RouterFunction<ServerResponse> routes(final ResourceBundleHandler handler) {
-        route(GET("/nedbank/bpm/resourcebundle/{key}"))
+        return route(GET("/nedbank/bpm/resourcebundle/{key}"), handler::getResourceBundleEntry);
+    }
+
+    @Bean
+    public ReloadableResourceBundleMessageSource resourceBundle(
+            @Value("${spring.cloud.config.uri}") final String uri) {
+        final String resourceBundleUrl = String.format("%s/locale/messages", uri);
+
+        final ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename(resourceBundleUrl);
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+
+        return messageSource;
     }
 }
