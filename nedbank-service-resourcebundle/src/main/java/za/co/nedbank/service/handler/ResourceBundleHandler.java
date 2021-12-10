c031b6f97deb55af730b8972460d037a7b5f84e3
Philip Lourandos <philip.lourandos@gmail.com>
2021-12-10 12:13:53 +0200

    * Added webflux handler

---
+++ nedbank-service-resourcebundle/src/main/java/za/co/nedbank/service/handler/ResourceBundleHandler.java
@@ -0,0 +1,11 @@
+package za.co.nedbank.service.handler;
+
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+public class ResourceBundleHandler {
+    public Mono<ServerResponse> getResourceBundleEntry(final ServerRequest request) {
+        
+    }
+}
