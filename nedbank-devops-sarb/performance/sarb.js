44ba37eb81134e8d7e523913a88d26e3fe6eb27a
Philip Lourandos <philip.lourandos@gmail.com>
2021-12-13 19:48:07 +0200

    * Corrected script

--- nedbank-devops-sarb/performance/sarb.js
+++ nedbank-devops-sarb/performance/sarb.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import {check} from 'k6';
 
-const uri = 'http://127.0.0.1:9080/sarb/rate';
+const uri = 'http://127.0.0.1:8080/sarb/rate';
 
 //Options for k6 to use while running tests
 export const options = {
@@ -10,19 +10,20 @@ export const options = {
 		http_req_duration: ['p(90) < 10'] //90% of all calls should be less than 10ms
 	},
 	scenarios: {
-		default: {
+		constRate: {
 			executor: 'constant-arrival-rate', //What the scenario is
 			duration: '100s', // How long this scenario will last
-			rate: 130, //Number of requests per seconds
-			preAllocatedVUs: 100 //Virtual users: 100 threads
+			rate: 2030, //Number of requests per seconds
+			preAllocatedVUs: 20, //Virtual users: 100 threads
+			maxVUs: 250
 		}
 	}
 }
 
 export default function() {
-	response = http.get(uri+'/repo');
+	const response = http.get(uri+'/repo');
 	
 	check(response, {
-		'succesful request': (r) => r.status === 200;
-	})
+		'succesful request': (r) => r.status === 200
+	});
 }
