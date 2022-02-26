506c83653239bd0e908ed9636d6ca695342b209e
Philip Lourandos <philip.lourandos@gmail.com>
2022-02-26 12:10:29 +0200

    #29
    * Script to build all the container images

---
+++ nedbank-devops/mock-services/build.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import os
+import os.path
+import subprocess
+
+for subdir in os.listdir("."):
+    if os.path.isdir(subdir):
+        dockerCmd = subprocess.Popen(['docker', 'build', '-t', "nedbank/bpmxjee-" + subdir + ":latest", '-f', 'Dockerfile', subdir])
+
