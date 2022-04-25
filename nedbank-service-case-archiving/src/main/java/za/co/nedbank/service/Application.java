cae87ef4b5c4f9a2463b68a51bef7b6d573e26d1
lore <lore@lore-VirtualBox>
2022-04-25 21:01:59 +0200

    #24
    * Added skeleton for initial start of project

---
+++ nedbank-service-case-archiving/src/main/java/za/co/nedbank/service/Application.java
@@ -0,0 +1,18 @@
+package za.co.nedbank.service;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableBatchProcessing
+@Configuration
+public class Application {
+
+    
+    
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
